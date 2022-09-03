c556ab24dc5e983322655bb6eef6561c492b7db1
YaroslavDrovolskyi <78113965+YaroslavDrovolskyi@users.noreply.github.com>
2022-09-03 16:18:49 +0300

    Minor changes

--- lab1/main.rb
+++ lab1/main.rb
@@ -10,7 +10,7 @@ b = gets.to_f
 print "Enter c: "
 c = gets.to_f
 
-print "Enter phi:"
+print "Enter phi: "
 phi = gets.to_f
 
 s = ((Math.sin(3*x))**3 + Math.atan(phi) - 6 * 10**3.1)/(Math.sqrt(a*(x**2) + b*x + c)) + Math.exp(x) * (Math.tan(x+2)).abs
