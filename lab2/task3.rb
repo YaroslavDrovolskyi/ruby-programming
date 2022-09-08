fcab61de8e2482393f9dffc359af35e23153e31d
YaroslavDrovolskyi <78113965+YaroslavDrovolskyi@users.noreply.github.com>
2022-09-08 20:43:52 +0300

    Added convertion for double and negative numbers

--- lab2/task3.rb
+++ lab2/task3.rb
@@ -1,20 +1,29 @@
 def convert_bin_to_dec(number_str)
     
-    if (number[0] < == '-') then return end
+    if (number_str[0] == '-') 
+        return (-1) * convert_bin_to_dec(number_str[1...number_str.length]) 
+    end
 
+    index_of_dot = number_str.index('.')
+    exp = (index_of_dot == nil) ? (number_str.length - 1) : (index_of_dot - 1)
     result = 0
-    n = number_str.length
-    for i in 0..n - 1
+    for i in 0...number_str.length
+        if number_str[i] == '.' then next end # skip dot
         digit = number_str[i].to_i
-        result += digit * (2 ** (n - 1 - i))
+        result += digit * (2 ** exp)
+        exp -= 1
     end 
-    
     return result
 end
 
 
 def main
-    puts convert_bin_to_dec("1000010001")
+    result = convert_bin_to_dec("1000010001")
+    if (result.is_a?(Integer) == false)
+        result = result.to_f
+    end
+
+    puts result
 end
 
 
