49599cf4ae55a5ba6e87221b775cbbc00422d63b
YaroslavDrovolskyi <78113965+YaroslavDrovolskyi@users.noreply.github.com>
2022-10-03 20:38:12 +0300

    IMplemented lab5 task2 ,added using lambdas

--- lab5/task1.rb
+++ lab5/task1.rb
@@ -1,11 +1,12 @@
 # n is number of intevals - analogue of accuracy
 # the bigger n is more accurate
+# assuming f parameter is lambda
 def prm(a, b, f, n)
     h = (b - a).to_f/n
     sum = 0
     1.upto(n){|i|
         x_i = a + i*h - h/2.0 
-        sum += method(f).call(x_i)
+        sum += f.(x_i)
     }
     sum *= h
 
@@ -18,15 +19,16 @@ def trp(a, b, f, n)
     sum = 0
 
     (a..b).step(h){|x|
-        sum += method(f).call(x)
+        sum += f.(x)
     }
-    sum -= method(f).call(a).to_f/2
-    sum -= method(f).call(b).to_f/2
+    sum -= f.(a).to_f/2
+    sum -= f.(b).to_f/2
     sum *= h
 
     return sum
 end
 
+=begin
 def f1(x)
     return 1.to_f / ((x + 1)**(1.5))
 end
@@ -34,19 +36,28 @@ end
 def f2(x)
     return (Math.tan(x.to_f/2 + Math::PI.to_f/4))**3
 end
+=end
+
+# define lambdas as a global variables
+
+$f1 = ->(x){
+    return 1.to_f / ((x + 1)**(1.5))
+}
+
+$f2 = ->(x){
+    return (Math.tan(x.to_f/2 + Math::PI.to_f/4))**3
+}
+
 
 def display(a1, b1, a2, b2, n)
     puts "\n\nIntegrate f1 from #{a1} to #{b1}, n = #{n}"
-    print "Rectangle method: ", prm(a1, b1, :f1, n), "\n"
-    print "  Trapeze method: ", trp(a1, b1, :f1, n), "\n\n"
+    print "Rectangle method: ", prm(a1, b1, $f1, n), "\n"
+    print "  Trapeze method: ", trp(a1, b1, $f1, n), "\n\n"
 
 
     puts "Integrate f2 from #{a2} to #{b2}, n = #{n}"
-    print "Rectangle method: ", prm(a2, b2, :f2, n), "\n"
-    print "  Trapeze method: ", trp(a2, b2, :f2, n), "\n\n\n"
-
-
-
+    print "Rectangle method: ", prm(a2, b2, $f2, n), "\n"
+    print "  Trapeze method: ", trp(a2, b2, $f2, n), "\n\n\n"
 end
 
 def main
